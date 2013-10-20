42b02551a1b272b54ee2af353f3c27dee2203046
Logan Hasson <logan.hasson@gmail.com>
2013-10-20 16:36:33 -0400

    Fix bad path in environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,9 @@
 require 'sqlite3'
 require 'erb'
 
-Dir.foreach("lib/concerns") do |file|
+Dir.foreach("lib/models/concerns") do |file|
   next if file.start_with?('.')
-  require_relative "../lib/concerns/#{file}" if file.end_with?(".rb")
+  require_relative "../lib/models/concerns/#{file}" if file.end_with?(".rb")
 end
 
 Dir.foreach("lib/models") do |file|
