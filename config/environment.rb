002f7214887d0b50cb23e7edb01d6f72278def16
Logan Hasson <logan.hasson@gmail.com>
2013-10-20 16:35:37 -0400

    Set up environment.rb and spec files

--- config/environment.rb
+++ config/environment.rb
@@ -1,1 +1,12 @@
 require 'sqlite3'
+require 'erb'
+
+Dir.foreach("lib/concerns") do |file|
+  next if file.start_with?('.')
+  require_relative "../lib/concerns/#{file}" if file.end_with?(".rb")
+end
+
+Dir.foreach("lib/models") do |file|
+  next if file.start_with?('.')
+  require_relative "../lib/models/#{file}" if file.end_with?(".rb")
+end
